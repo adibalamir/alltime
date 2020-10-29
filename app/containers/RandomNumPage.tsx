561def0c5ffee77dc4154bdb8de188840b85f98d
Adih <adibalamir@hotmail.com>
2020-10-29 15:20:38 -0400

    Add page for randomnumgenerator.

---
+++ app/containers/RandomNumPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RandomNumGenerator from '../features/randomnum/RandomNumGenerator';
+
+export default function RandomNumGeneratorPage() {
+  return <RandomNumGenerator />;
+}
